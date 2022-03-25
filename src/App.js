a6b2aedbf114c3de9d77dc784ff9d5665ff7b875
ahmed nasser <ahmednas197@gmail.com>
2022-03-25 16:11:12 +0200

    add inventory component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Inventory from './Components/Inventory';
+import Pricing from './Components/Pricing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='box'>
+        <Inventory></Inventory>
+        <Pricing></Pricing>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Inventory.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+export default function Inventory() {
+    const [text, setText] = useState("")
+    return (
+        <div>
+            <h5>Inventory</h5>
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="inputSKU">SKU (Stock Keeping Unit) </label>
+                        <input type="text" className="form-control" id="inputSKU" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label for="inputBarcode">Barcode (ISBN,UPC,GTIN,etc.)</label>
+                        <input type="text" className="form-control" id="inputBarcode" placeholder="" />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck-1" />
+                        <label className="form-check-label" for="gridCheck-1">
+                            Track quantity
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck-2" />
+                        <label className="form-check-label" for="gridCheck-2">
+                            Continue selling when out of stock
+                        </label>
+                    </div>
+                </div>
+                <div className='line'>
+                </div>
+                <h6>QUANTITY</h6>
+
+
+
+                <div class="form-group row">
+                    <div class="col-md-6">
+                        <label for="apples">Available </label>
+                        <input type="number" class="form-control" id="apples" autocomplete="off"
+                            min="0" placeholder='0' />
+                    </div>
+
+                </div>
+
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="inputSKU">Title</label>
+                        <input type="text" className="form-control" id="inputSKU" placeholder="Short sleeve t-shirt" />
+                    </div>
+                </div>
+
+                <div className="form">
+                    <div className="editor">
+                        <CKEditor
+                            editor={ClassicEditor}
+                            data={text}
+                            onChange={(event, editor) => {
+                                const data = editor.getData()
+                                setText(data)
+                            }}
+                        />
+                    </div>
+                </div>
+
+            </form >
+        </div >
+    )
+}
